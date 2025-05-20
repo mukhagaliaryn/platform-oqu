2468dbee844db224252ec8f0017a052d2c72aa3c
mukhagaliaryn <mukhagaliaryn@gmail.com>
2025-05-20 18:04:28 +0500

    Done

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,11 +3,11 @@ import MainLayout from '@/layouts/main-layout';
 
 
 export default async function MainPage() {
-    const res = await fetch('http://127.0.0.1:8000/');
+    const res = await fetch(`${process.env.BACKEND_URL}/`);
     const data = await res.json();
     const courses = data.courses;
     const blogs = data.blogs;
-    
+
     return (
         <MainLayout>
             <LandingSection courses={courses} blogs={blogs} />
